09926363de4bba1708f3b876d0f11b4941beeddf
t883w2 <t883w2@bma.biglobe.ne.jp>
2018-08-05 22:51:44 +0900

    first monday

--- eitango/ViewController.swift
+++ eitango/ViewController.swift
@@ -1,20 +1,54 @@
-//
 //  ViewController.swift
 //  eitango
-//
-//  Created by ginzo on 2018/08/04.
-//  Copyright © 2018年 ginzo. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tf: UITextField!
+   // let tango:[Int:[String:String]] = [["月曜日":"monday"],["火曜日":"tuesday"],["水曜日":"wednesday"],["木曜日":"thursday"],["金曜日":"friday"],["土曜日":"saturday"],["日曜日":"sunday"]]
+    var tango = [["月曜日": "monday"],["火曜日":"tuesday"],["水曜日":"wednesday"],["木曜日":"thursday"],["金曜日":"friday"],["土曜日":"saturday"],["日曜日":"sunday"]]
+    var tkey = ""
+    var tarr =  [String: String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      label.layer.masksToBounds=true
+        label.layer.cornerRadius = 12
+        label.layer.borderWidth = 1
+        shaffle()
     }
+    
+    
+    
+    func shaffle(){
+        let i = Int(arc4random_uniform(7))
+        tarr = tango[i]
+        tkey = Array(tarr.keys)[0]
+        label.text = tkey
+        
+    }
+    
+    
+    
 
+    @IBAction func button(_ sender: Any) {
+        print(tarr[tkey]!)
+        if tarr[tkey]! == tf.text!{
+            label.text = tkey + " 正解!"
+        }else{
+            label.text = tkey + " はずれ!"
+
+        }
+        
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
